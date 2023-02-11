1f5ebb3d481fbd3af4700fa50f0baeb53610b2fd
Waldemar Tomme <waldetomme@gmail.com>
2023-02-11 05:18:42 +0100

    Generate single extension method

--- DarkLink.Roslyn.Asyncify.Test/GeneratorTest.cs
+++ DarkLink.Roslyn.Asyncify.Test/GeneratorTest.cs
@@ -4,18 +4,11 @@ namespace DarkLink.Roslyn.Asyncify.Test;
 public class GeneratorTest : VerifySourceGenerator
 {
     [TestMethod]
-    public async Task Empty()
-    {
-        var source = string.Empty;
-
-        await Verify(source);
-    }
-
-    [TestMethod]
     public async Task AsyncifyParameterLessVoidMethod()
     {
         var source = @"
 using System;
+using System.Threading.Tasks;
 using DarkLink.Roslyn;
 
 internal class Subject
@@ -25,7 +18,20 @@ internal class Subject
 
 [Asyncify(typeof(Subject), nameof(Subject.CallMe))]
 internal static partial class Extensions { }
+
+internal static class Program
+{
+    public static Task Main() => Task.FromResult(new Subject()).CallMe();
+}
 ";
+
+        await Verify(source);
+    }
+
+    [TestMethod]
+    public async Task Empty()
+    {
+        var source = string.Empty;
 
         await Verify(source);
     }
